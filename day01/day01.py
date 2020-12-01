7415c8d83f829a3220bc7184dac9c7e21132b81f
Christoph Buttler <christoph.buttler@outlook.com>
2020-12-01 12:59:27 +0100

    add day01

---
+++ day01/day01.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import sys
+
+entries = []
+with open("day01/day01.txt", "r") as f:
+    entries = f.readlines()
+    entries = [int(entry.strip()) for entry in entries]
+
+# puzzle 1
+for (i, x) in enumerate(entries):
+    tempEntries = entries[:i] + entries[(i + 1):]
+    result = list(filter(lambda y: (x + y) == 2020, tempEntries))
+    if(len(result) > 0):
+        y = result.pop()
+        print("{:d} + {:d} = 2020".format(x, y))
+        z = x * y
+        print("{:d} * {:d} = {:d}".format(x, y, z))
+        break
+
+# puzzle 2
+for (i, x) in enumerate(entries):
+    tempEntriesX = entries[:i] + entries[(i + 1):]
+    for (j, y) in enumerate(tempEntriesX):
+        tempEntriesY = tempEntriesX[:j] + tempEntriesX[(j + 1):]
+        result = list(filter(lambda z: (x + y + z) == 2020, tempEntriesY))
+        if(len(result) > 0):
+            z = result.pop()
+            print("{:d} + {:d} + {:d} = 2020".format(x, y, z))
+            a = x * y * z
+            print("{:d} * {:d} * {:d} = {:d}".format(x, y, z, a))
+            sys.exit(1)

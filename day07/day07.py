f6291e17e79972622649bddf676d58ebf3bddedc
Christoph Buttler <christoph.buttler@outlook.com>
2020-12-11 18:00:27 +0100

    add day07 puzzle 1

---
+++ day07/day07.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import sys
+import re
+
+
+def parseLine(currLine: str) -> (str, dict):
+    (bagType, bagRulesString) = [x.strip() for x in currLine.split("bags contain")]
+    bagRules = dict()
+
+    if "no other bags" in bagRulesString:
+        return (bagType, bagRules)
+
+    bagRulesRegex = r"(\d+)\s+([a-zA-Z\s]+)bags?"
+    for currRule in re.finditer(bagRulesRegex, bagRulesString):
+        (currAmount, currBagType) = [x.strip() for x in currRule.groups()]
+        bagRules[currBagType] = int(currAmount)
+
+    return (bagType, bagRules)
+
+
+def canContainBag(bags: dict, targetBagType: str, baseBagType: str) -> bool:
+    if len(bags[baseBagType]) == 0:
+        return False
+
+    if bags[baseBagType].get(targetBagType) != None:
+        return True
+
+    for nextBaseBagType in bags[baseBagType].keys():
+        if canContainBag(bags, targetBagType, nextBaseBagType):
+            return True
+
+    return False
+
+
+if len(sys.argv) != 2:
+    print("Usage: python <path-to-input-data>")
+    sys.exit(1)
+
+bags = dict()
+with open(sys.argv[1], "r") as f:
+    bags.update([parseLine(line) for line in f.readlines()])
+
+# puzzle 1
+bagCount = 0
+for currBagType in bags.keys():
+    bagCount += canContainBag(bags, "shiny gold", currBagType)
+
+print("Puzzle 1 count:", bagCount)

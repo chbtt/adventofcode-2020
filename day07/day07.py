9235b7647ec97395f7ee7b4f8e0c1b4997e821a5
Christoph Buttler <christoph.buttler@outlook.com>
2020-12-12 03:09:39 +0100

    add day07 puzzle 2

--- day07/day07.py
+++ day07/day07.py
@@ -32,6 +32,17 @@ def canContainBag(bags: dict, targetBagType: str, baseBagType: str) -> bool:
     return False
 
 
+def getBagCount(bags: dict, targetBagType: str) -> int:
+    bagCount = 0
+    for currBagType in bags[targetBagType].keys():
+        # the bags of currBagType themselves
+        bagCount += bags[targetBagType][currBagType]
+        # the bags inside of currBagType
+        bagCount += bags[targetBagType][currBagType] * getBagCount(bags, currBagType)
+
+    return bagCount
+
+
 if len(sys.argv) != 2:
     print("Usage: python <path-to-input-data>")
     sys.exit(1)
@@ -46,3 +57,8 @@ for currBagType in bags.keys():
     bagCount += canContainBag(bags, "shiny gold", currBagType)
 
 print("Puzzle 1 count:", bagCount)
+
+# puzzle 2
+bagCount = getBagCount(bags, "shiny gold")
+
+print("Puzzle 2 count:", bagCount)
